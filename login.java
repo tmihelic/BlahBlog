02f866ab51f06304705615db8186d6b91aae4611
tmihelic <tmihelic@mail.umw.edu>
2012-11-19 14:04:51 -0500

    Update login.java
    Added input

--- login.java
+++ login.java
@@ -4,4 +4,11 @@ import java.util.Scanner;
 
 public class login{
 
+System.out.print("Enter Username: ");
+String username = input.nextLine();
+System.out.print("Enter Password: ");
+String password = Input.nextLine();
+
+System.out.print("Your Username: " + username);
+System.out.print("Your Password: " + password);
 }
