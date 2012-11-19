18e1657f95b831541a7b50f0b6df72f8c2b22c85
tmihelic <tmihelic@mail.umw.edu>
2012-11-19 14:50:46 -0500

    Update login.java
    Added to login, most of code still commented out

--- login.java
+++ login.java
@@ -1,14 +1,81 @@
 import javax.swing.*;
 import java.awt.event.*;
 import java.util.Scanner;
+import java.io.*;
 
 public class login{
+
+ public static void main(String[] args) {
+
+Scanner input = new Scanner(System.in);
 
 System.out.print("Enter Username: ");
 String username = input.nextLine();
 System.out.print("Enter Password: ");
-String password = Input.nextLine();
+String password = input.nextLine();
 
-System.out.print("Your Username: " + username);
-System.out.print("Your Password: " + password);
+System.out.print("Your Username: " + username + '\n');
+System.out.println("Your Password: " + password);
+
+//FileInputStream fis = new FileInputStream("login.blah");
+//BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+String s = username + " " + password;
+
+System.out.print(s);
+s = br.readLine();
+
 }
+
+return username;
+}
+
+/*
+class  file
+{
+public static void main(String arg[])throws Exception
+{
+FileInputStream fis=new	FileInputStream("a.txt");
+
+String s;
+char ch;
+int i,j,exit=0,flag=0;
+boolean x=true;
+System.out.println("Enter the	string to be found");
+s=br.readLine();
+while(x)
+{
+j=0;
+for(i=0;i<s.length();i++)
+{
+ch=((char)fis.read());
+if(ch==-1)
+{
+exit=1;
+System.out.println(exit+"for");
+break;
+}
+if(ch==s.charAt(i))
+{
+j++;
+}
+}
+if(exit==1)
+{
+System.out.println(exit+"while");
+break;
+}
+if(j==s.length())
+{
+flag=1;
+System.out.println("Success");
+break;
+}
+}
+if(flag==1)
+System.out.println("The	string is found");
+else
+System.out.println("The	string is not found");
+}
+}
+*/	
