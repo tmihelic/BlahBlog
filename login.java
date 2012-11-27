14e88db65356040100e2845fff0b3e99b415e99b
tmihelic <tmihelic@mail.umw.edu>
2012-11-27 01:21:34 -0500

    Update login.java
    Login is completely functional, need to just change 3 minor things when syncing up with rest of program (commented in code)

--- login.java
+++ login.java
@@ -1,81 +1,45 @@
-import javax.swing.*;
-import java.awt.event.*;
 import java.util.Scanner;
 import java.io.*;
 
 public class login{
+ 
+ //needs to be commented out 
+ //(added to run on own)
+ public static void main(String[] args) throws Exception{
 
- public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
 
-Scanner input = new Scanner(System.in);
 
-System.out.print("Enter Username: ");
-String username = input.nextLine();
-System.out.print("Enter Password: ");
-String password = input.nextLine();
+		System.out.print("Enter Username: ");
+		String username = input.nextLine();
+		System.out.print("Enter Password: ");
+		String password = input.nextLine();
+		//combines username and password into a single string
+		String s = username + " " + password;
 
-System.out.print("Your Username: " + username + '\n');
-System.out.println("Your Password: " + password);
 
-//FileInputStream fis = new FileInputStream("login.blah");
-//BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		try{
+			//selects file login.txt to be read
+   //(need to change name of file)
+			FileReader file1 = new FileReader(new File("/Users/hawaiian242/Documents/CPSC-330/login.txt"));
+			//reads file
+			BufferedReader f = new BufferedReader(file1);
+			//creates temp string file to save each line read
+			String temp;
+			Boolean found = false;
 
-String s = username + " " + password;
-
-System.out.print(s);
-s = br.readLine();
-
+			while((temp=f.readLine())!=null && found == false)
+			{
+				if(temp.equals(s))
+				{
+					System.out.println("It Worked");
+					found = true;
+				}
+			}
+		}
+		catch(Exception e)
+		{System.out.println("Error Reading Login file");}
+	}
+ //(return statement needs to be uncommented out)
+	//return username;
 }
-
-return username;
-}
-
-/*
-class  file
-{
-public static void main(String arg[])throws Exception
-{
-FileInputStream fis=new	FileInputStream("a.txt");
-
-String s;
-char ch;
-int i,j,exit=0,flag=0;
-boolean x=true;
-System.out.println("Enter the	string to be found");
-s=br.readLine();
-while(x)
-{
-j=0;
-for(i=0;i<s.length();i++)
-{
-ch=((char)fis.read());
-if(ch==-1)
-{
-exit=1;
-System.out.println(exit+"for");
-break;
-}
-if(ch==s.charAt(i))
-{
-j++;
-}
-}
-if(exit==1)
-{
-System.out.println(exit+"while");
-break;
-}
-if(j==s.length())
-{
-flag=1;
-System.out.println("Success");
-break;
-}
-}
-if(flag==1)
-System.out.println("The	string is found");
-else
-System.out.println("The	string is not found");
-}
-}
-*/	
